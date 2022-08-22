2ad31861ec40ad7c8fd923e8a3a2ca1ce5c69987
rafeed035 <shahed.rafeed@gmail.com>
2022-08-22 15:25:00 +0600

    xml based bean life cycle implemented

---
+++ src/main/java/Entity/Lab.java
@@ -0,0 +1,38 @@
+package Entity;
+
+public class Lab {
+    private int labId;
+    private String labName;
+
+    public Lab(int labId, String labName) {
+        this.labId = labId;
+        this.labName = labName;
+    }
+
+    public Lab() {
+    }
+
+    public int getLabId() {
+        return labId;
+    }
+
+    public String getLabName() {
+        return labName;
+    }
+
+    @Override
+    public String toString() {
+        return "Lab{" +
+                "labId=" + labId +
+                ", labName='" + labName + '\'' +
+                '}';
+    }
+
+    public void init(){
+        System.out.println("Inside the init method of lab class");
+    }
+
+    public void destroy(){
+        System.out.println("Inside the destroy method of the lab class");
+    }
+}
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -5,38 +5,50 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainClass {
     public static void main(String[] args) {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("collectionconfig.xml");
-//        ApplicationContext applicationContext1 = new ClassPathXmlApplicationContext("referenceconfig.xml");
-//        ApplicationContext applicationContext2 = new ClassPathXmlApplicationContext("constructorConfig.xml");
-//        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext("beanLifeCycleInterface.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("collectionconfig.xml");
+        ApplicationContext applicationContext1 = new ClassPathXmlApplicationContext("referenceconfig.xml");
+        ApplicationContext applicationContext2 = new ClassPathXmlApplicationContext("constructorConfig.xml");
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext("beanLifeCycleInterface.xml");
         AbstractApplicationContext abstractApplicationContext1 = new ClassPathXmlApplicationContext("beanLifeCycleAnnotations.xml");
-//        abstractApplicationContext.registerShutdownHook();
+        AbstractApplicationContext abstractApplicationContext2 = new ClassPathXmlApplicationContext("beanLifeCycleXml.xml");
+        abstractApplicationContext.registerShutdownHook();
         abstractApplicationContext1.registerShutdownHook();
+        abstractApplicationContext2.registerShutdownHook();
 
-//        Student student1 = (Student) context.getBean("student1");
-//        System.out.println(student1);
-//
-//        Student student2 = (Student) context.getBean("student2");
-//        System.out.println(student2);
-//
-//        Student student3 = (Student) context.getBean("student3");
-//        System.out.println(student3);
-//
-//        Teacher teacher1 = (Teacher) applicationContext.getBean("teacher1");
-//        System.out.println(teacher1);
-//
-//        Course course = (Course) applicationContext1.getBean("course1");
-//        System.out.println(course);
-//
-//        Department department = (Department) applicationContext2.getBean("department1");
-//        System.out.println(department);
-//
-//        Building building = (Building) abstractApplicationContext.getBean("building1");
-//        System.out.println(building);
+        Student student1 = (Student) context.getBean("student1");
+        System.out.println(student1);
 
-        Staff staff = (Staff) abstractApplicationContext1.getBean("staff1");
-        System.out.println(staff);
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println();
+        System.out.println(student2);
+
+        Student student3 = (Student) context.getBean("student3");
+        System.out.println();
+        System.out.println(student3);
+
+        Teacher teacher1 = (Teacher) applicationContext.getBean("teacher1");
+        System.out.println();
+        System.out.println(teacher1);
+
+        Course course = (Course) applicationContext1.getBean("course1");
+        System.out.println();
+        System.out.println(course);
+
+        Department department = (Department) applicationContext2.getBean("department1");
+        System.out.println();
+        System.out.println(department);
+
+        Building building = (Building) abstractApplicationContext.getBean("building1");
+        System.out.println();
+        System.out.println(building);
+
+//        Staff staff = (Staff) abstractApplicationContext1.getBean("staff1");
+//        System.out.println(staff);
+
+        Lab lab = (Lab) abstractApplicationContext2.getBean("lab1");
+        System.out.println(lab);
+
 
     }
 }
